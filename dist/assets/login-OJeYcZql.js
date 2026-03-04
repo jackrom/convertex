6d4bfccf32e1e42d4a3f786ecc7aa46a1512e0ba
jrc2 <juan.reyes@coremain.com>
2026-03-04 03:36:55 -0500

    Se agrega build (dist)

---
+++ dist/assets/login-OJeYcZql.js
@@ -0,0 +1,1 @@
+import{E as e,c as o,o as a}from"./index-BBGBe4Ym.js";const c={class:"pa-6"},_={__name:"login",setup(n){return e(async()=>{}),(s,t)=>(a(),o("div",c,"Redirigiendo a Keycloak..."))}};export{_ as default};

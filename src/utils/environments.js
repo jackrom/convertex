e4852cdc13e817f4146a864dde04f5422a099543
jrc2 <juan.reyes@coremain.com>
2026-03-04 03:25:49 -0500

    Se hacen cambios en environments

--- src/utils/environments.js
+++ src/utils/environments.js
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000',
+  // apiUrl: 'http://localhost:4000',
 
-  // apiUrl: 'https://apifc.dev.facilcontabilidad.org',
+  apiUrl: 'https://apifc.dev.facilcontabilidad.org',
 }

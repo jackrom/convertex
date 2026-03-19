8ce0b328b484ae7004b9d10285fcd8293feb7c40
Juan Carlos Reyes <jackrom@live.com>
2026-03-18 21:43:59 -0500

    Se agrega build (dist)

---
+++ src/financialEngine/formulas/eri.formulas.js
@@ -0,0 +1,23 @@
+export const ERI_FORMULAS = {
+
+  eri_502: "eri_50201 + eri_50202 + eri_50203 + eri_50204",
+
+  eri_600: "eri_401 - eri_501 - eri_502 + eri_403",
+
+  eri_602: "eri_600 + eri_601",
+
+  eri_604: "eri_602 + eri_603",
+
+  eri_607: "eri_604 + eri_605 + eri_606",
+
+  eri_702: "eri_700 - eri_701",
+
+  eri_704: "eri_702 - eri_703",
+
+  eri_706: "eri_704 - eri_705",
+
+  eri_707: "eri_607 + eri_706",
+
+  eri_801: "eri_707 + eri_800",
+
+}

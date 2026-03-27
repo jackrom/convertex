b86934e986537cff22e70a52791eb299da7447ea
Juan Carlos Reyes <jackrom@live.com>
2026-03-27 14:07:07 -0500

    correcciones directas en repoortes txt

---
+++ dist/assets/SoporteList-DRf_kFXP.js
@@ -0,0 +1,1 @@
+import{c as t,q as o,d as a,o as r}from"./index-Cy7sQUdv.js";const l={__name:"SoporteList",setup(s){return console.log("SOPORTE"),(n,e)=>(r(),t("p",null,[...e[0]||(e[0]=[o("Ir a ",-1),a("a",{href:"https://facilcontabilidad.net/"},"Soporte",-1)])]))}};export{l as default};

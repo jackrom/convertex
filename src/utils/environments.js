825221ae2860fa0bf6f80b731aee99fe4328bc96
jrc2 <juan.reyes@coremain.com>
2026-04-08 19:41:34 -0500

    rediseño visual

--- src/utils/environments.js
+++ src/utils/environments.js
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4001',
-  baseUrl: 'http://localhost:5173',
+  // apiUrl: 'http://localhost:4001',
+  // baseUrl: 'http://localhost:5173',
 
-  // apiUrl: 'https://apifc.dev.facilcontabilidad.org',
+  piUrl: 'https://apifc.dev.facilcontabilidad.org',
+  baseUrl: 'https://convertex.facilcontabilidad.org',
 }
